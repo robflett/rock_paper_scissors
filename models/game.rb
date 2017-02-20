a1c38126be565c7b0898905131ca70f3353224e0
Rob <robflettcc@gmail.com>
2017-02-20 20:21:58 +0000

    Changes to appearance and discovered a flaw in the game logic q ;

--- models/game.rb
+++ models/game.rb
@@ -11,10 +11,19 @@ def play_round()
 
   if @hand1 == @hand2
     return "It's a draw!"
-  elsif (@hand1 == "Paper" && @hand2 == "Rock") 
-  (@hand1 == "Rock" && @hand2 == "Scissors")  
-  (@hand1 == "Scissors" && @hand2 == "Paper")
-    return "#{@hand1} wins!"
+
+  elsif 
+    (@hand1 == "Paper" && @hand2 == "Rock") 
+  return "#{@hand1} wins!"
+
+  elsif
+    (@hand1 == "Rock" && @hand2 == "Scissors")  
+  return "#{@hand1} wins!"
+
+  elsif
+    (@hand1 == "Scissors" && @hand2 == "Paper")
+  return "#{@hand1} wins!"
+
   else
     return "#{@hand2} wins!"
   end

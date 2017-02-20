d4b0cd8ac4f1fbd0c854824dbef3986e8ca2b97f
Rob <robflettcc@gmail.com>
2017-02-20 20:03:36 +0000

    Added further functions - erb, css

--- controller.rb
+++ controller.rb
@@ -4,7 +4,16 @@ require('pry') #useful for debugging
 require('./models/game') #link to any class. This exposes it to the www
 require('json') 
 
-get 'play_round/:hand1/:hand2' do
+get '/play_round/:hand1/:hand2' do
   game = Game.new(params[:hand1], params[:hand2])
-  return "#{game.play_round()}"
+  @result = game.play_round()
+  erb( :result )
+end
+
+get '/' do
+    erb( :welcome )
+end
+
+get '/how_to_play' do 
+    erb( :how_to_play )
 end
--- models/game.rb
+++ models/game.rb
@@ -11,8 +11,8 @@ def play_round()
 
   if @hand1 == @hand2
     return "It's a draw!"
-  elsif (@hand1 == "Paper" && @hand2 == "Rock") ||
-  (@hand1 == "Rock" && @hand2 == "Scissors")  ||
+  elsif (@hand1 == "Paper" && @hand2 == "Rock") 
+  (@hand1 == "Rock" && @hand2 == "Scissors")  
   (@hand1 == "Scissors" && @hand2 == "Paper")
     return "#{@hand1} wins!"
   else
